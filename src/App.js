aab0b5ea133fa3a99e3db70772781b7b8514f416
jinenab <Jinen Abdelhak>
2021-03-28 03:32:33 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
 
   const handleClick = () => {
-    //const tab = todos;
-    todos.push(todo);
+    setTodos([...todos, todo]);
+    setTodo('');
   };
   const handleDelete = (indexToDelete) => {
     const tab = todos;
     tab.filter((e, index) => index !== indexToDelete);
     setTodos(tab);
   };
+  // useEffect(() => {}, [todos]);
 
   return (
     <div className='App'>
       <input
+        value={todo}
         onChange={(e) => {
           setTodo(e.target.value);
         }}
       />
       <button onClick={handleClick}>Add</button>
-      <div>
-        {todos.length > 0 &&
-          todos.map((e, index) => (
-            <div key={index}>
-              <p>{e}</p>
-              <button value={e} onClick={handleDelete(index)}>
-                Delete
-              </button>
-            </div>
-          ))}
-      </div>
+      <ul>
+        {todos && todos.length > 0
+          ? todos.map((e, index) => (
+              <li key={index}>
+                <p>{e}</p>
+                <button onClick={handleDelete(index)}>Delete</button>
+              </li>
+            ))
+          : 'No items'}
+      </ul>
     </div>
   );
 }
